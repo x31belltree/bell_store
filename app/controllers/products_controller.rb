5879212a81332021c0303f680e7772aaf93dacbf
x31belltree <belltree.masaki@gmail.com>
2020-07-27 10:58:28 +0000

    検索機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all.page(params[:page])
+    @q_product = Product.ransack(params[:q])
+    @products = @q_product.result.page(params[:page])
   end
 
   # GET /products/1
