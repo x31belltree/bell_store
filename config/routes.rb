8ced4b614fa6b1a2e12d295d987b9a7505ecc4ce
x31belltree <belltree.masaki@gmail.com>
2020-08-17 08:36:06 +0000

    ヘルパーメソッド、carts コントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
+  
+  resources :carts, only: [:show]
+
+  post '/add_item' => 'carts#add_item'
+  post '/update_item' => 'carts#update_item'
+  delete '/delete_item' => 'carts#delete_item'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
