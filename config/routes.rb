d645ca7d8053a653092308f7ad0d940d18b4b6f3
x31belltree <belltree.masaki@gmail.com>
2020-08-27 10:58:42 +0000

    カートを空にするボタンを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
-  resources :cart_items
+  resources :cart_items, only: [:index, :create, :update, :destroy] do
+    collection do
+        delete 'destroy_all'
+    end
+  end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
